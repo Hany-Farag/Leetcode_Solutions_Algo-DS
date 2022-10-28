bbe2c44905ffcb9291079f4a346b099a6e2fa29d
Hany Adel Attala Farag <81488455+Hany-Farag@users.noreply.github.com>
2022-10-29 00:17:31 +0200

    Update 0189-rotate-array.cpp

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -10,3 +10,24 @@ public:
        nums.erase(nums.begin(),nums.begin()+dif);
     }
 };
+/* Another solution with deque 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        deque<int> res;
+        for(auto i:nums)
+        {
+            res.push_back(i);
+        }
+        while(k--)
+        {
+            res.push_front(res.back());
+            res.pop_back();
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i]=res[i];
+        }
+    }
+};
+*/

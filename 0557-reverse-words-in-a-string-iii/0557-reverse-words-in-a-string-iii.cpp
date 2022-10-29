13262c8271216ad10934d52080c371a6c4c18910
Hany Adel Attala Farag <81488455+Hany-Farag@users.noreply.github.com>
2022-10-29 20:43:18 +0200

    Time: 19 ms (83.57%), Space: 10.9 MB (47.31%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string temp,res;
+        /*string temp,res;
         for(auto i:s)
         {
             if(i==' ')
@@ -16,6 +16,25 @@ public:
         reverse(temp.begin(),temp.end());
         res+=temp;
         
+        return res;*/
+        
+        int l=0,r=0,n=s.size();
+        string res;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' '||i==s.size()-1)
+            {
+                for(int j=(s[i]==' '?r-1:r);j>=l;j--)
+                {
+                    //(s[i]==' '?r--:r) because if s[i]==' ' 
+                    //so the r should reduce by one to end exactly on word 
+                    res+=s[j];
+                }
+                if(i!=s.size()-1) res+=' ';
+                l=i+1;
+            }
+            r++;
+        }
         return res;
     }
 };

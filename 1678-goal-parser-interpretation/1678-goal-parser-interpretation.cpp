10bb924d165c81ed9f93d04da9906bbf7e186d1a
Hany Adel Attala Farag <81488455+Hany-Farag@users.noreply.github.com>
2023-01-31 23:56:16 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (17.53%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string res,temp;
+        for(int i=0;i<command.size();i++)
+        {
+            if(command[i]=='G')res.push_back('G');
+            else if (command[i]=='('&&command[i+1]==')')
+            {
+                res.push_back('o');
+                i++;
+            }
+            else{
+                res+="al";
+                i+=3;
+            } 
+        }
+        return res;
+        
+    }
+};

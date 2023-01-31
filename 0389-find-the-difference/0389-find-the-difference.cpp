8ffaf88329972bd85cfed59f69e00f98b7dd64c1
Hany Adel Attala Farag <81488455+Hany-Farag@users.noreply.github.com>
2023-02-01 00:13:10 +0200

    Time: 4 ms (61.84%), Space: 6.9 MB (11.09%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());sort(t.begin(),t.end());
+      /*  sort(s.begin(),s.end());sort(t.begin(),t.end());
         for(int i=0;i<s.size();i++)
         {
             if(s[i]!=t[i])return t[i];
         }
         return t[t.size()-1];
-        
-        
+        compllexity nlog(n)+nlog(n)+n=nlog(n)*/
+    
+        map<char,int>mp;char res;
+        for(auto i:s)mp[i]++;
+        for(auto i:t)mp[i]++;
+        for(auto i:mp)
+            if(i.second%2)
+                return i.first;
+       return ' ';
     }
 };
